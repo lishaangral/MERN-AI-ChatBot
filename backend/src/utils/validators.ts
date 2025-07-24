fdf65607d19f6bac41fae24870cde3af9c9c1a71
lishaangral <lishaangral@gmail.com>
2025-07-24 22:37:13 +0530

    Name, Email & Password Express Validation

---
+++ backend/src/utils/validators.ts
@@ -0,0 +1,29 @@
+import { NextFunction, Request, Response } from 'express';
+import { body, ValidationChain, validationResult } from "express-validator";
+
+const validate = (validations: ValidationChain[]) => {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        for (let validation of validations) {
+            const result = await validation.run(req);
+            if (!result.isEmpty()) {
+                break;
+            }
+        }
+        const errors = validationResult(req);
+        if (errors.isEmpty()) {
+            return next();
+        }
+        return res.status(422).json({
+            message: "Validation failed",
+            errors: errors.array()
+        });
+    };
+};
+
+const signupValidator = [
+    body("name").notEmpty().withMessage("Name is required"),
+    body("email").trim().isEmail().withMessage("Email is required"),
+    body("password").trim().isLength({min: 6}).withMessage("Password must be at least 6 characters long"),
+];
+
+export {validate, signupValidator};
