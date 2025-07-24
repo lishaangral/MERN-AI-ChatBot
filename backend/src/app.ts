9330038d174a9a35fb0f4064c95b65df5fb7bb56
lishaangral <lishaangral@gmail.com>
2025-07-24 23:51:30 +0530

    creating token and storing cookies

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { config } from 'dotenv';
 import morgan from 'morgan';
 import appRouter from './routes/index.js';
+import cookieParser from 'cookie-parser';
 
 config();
 const app = express();
@@ -13,7 +14,7 @@ const app = express();
 
 // middleware to parse JSON bodies
 app.use(express.json());
-
+app.use(cookieParser(process.env.COOKIE_SECRET));
 // remove it during production
 app.use(morgan("dev"));
 
