60c9767403e63f123b5b8fd636acde1ce466c22c
lishaangral <lishaangral@gmail.com>
2025-09-14 03:38:44 +0530

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,10 +7,7 @@ import { AuthProvider } from './context/AuthContext'
 import {Toaster} from 'react-hot-toast';
 import axios from "axios";
 
-// use for dev only
-// const API_BASE = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api/v1";
-
-const API_BASE = "https://mern-ai-chatbot-backend-g62a.onrender.com";
+const API_BASE = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api/v1";
 axios.defaults.baseURL = API_BASE;
 axios.defaults.withCredentials = import.meta.env.VITE_API_USE_CREDENTIALS === "true" || true;
 
